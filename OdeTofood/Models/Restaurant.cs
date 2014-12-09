d7e310d4efd56d88a07f100cd9de8b9ab40e3c2a
LuisPolo <i820326@gmail.com>
2014-12-09 13:49:06 -0500

    Code expression @HTML

---
+++ OdeTofood/Models/Restaurant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeTofood.Models
+{
+   public class Restaurant
+    {
+
+        public string Name { get; set;}
+
+    }
+}
