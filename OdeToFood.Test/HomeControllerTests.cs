cd9acd7aff8a1e3994be7f1c304ee11325f46d6d
LuisPolo <i820326@gmail.com>
2014-12-08 20:11:39 -0500

    Inicio

---
+++ OdeToFood.Test/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OdeTofood.Controllers;
+using System.Web.Mvc;
+
+namespace OdeTofood.Test
+{
+     [TestClass]
+    public class HomeControllerIndexTests
+    {
+
+        [TestMethod]
+        public void Puts_Message_In_ViewBag() 
+        {
+            var controller = new HomeController();
+            ViewResult result = controller.Index();
+            Assert.IsNotNull(result.ViewBag.Message);
+
+        }
+
+    }
+}
---
+++ OdeTofood/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using OdeTofood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeTofood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            var model = new RestaurantReview();
+            model.Name = "Long Horn";
+            model.Rating = 9;
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Descripcion de la pagina";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
